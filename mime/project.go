7e3f582e72afaa05af6106ff1eee17dd43a418b4
BubbleFish <daforsastudia@gmail.com>
2026-01-12 20:56:53 +0300

    feat: Time building function and report it

--- mime/project.go
+++ mime/project.go
@@ -1,6 +1,11 @@
 package mime
 
-import "github.com/bbfh-dev/mime/mime/minecraft"
+import (
+	"time"
+
+	"github.com/bbfh-dev/mime/cli"
+	"github.com/bbfh-dev/mime/mime/minecraft"
+)
 
 type Project struct {
 	Meta *minecraft.PackMcmeta
@@ -13,5 +18,14 @@ func New(mcmeta *minecraft.PackMcmeta) *Project {
 }
 
 func (project *Project) Build() error {
+	start := time.Now()
+	cli.LogInfo(
+		false,
+		"Building v%s for Minecraft %s",
+		project.Meta.File.Get("meta.version"),
+		project.Meta.File.Get("meta.minecraft"),
+	)
+
+	cli.LogDone(false, "Finished building in %s", time.Since(start))
 	return nil
 }

f4bdb61a59eea6dc39fa49499b65604aec20bb45
BubbleFish <daforsastudia@gmail.com>
2026-01-25 13:29:19 +0300

    feat!: Delete --cache flag

--- cli/cli_main.go
+++ cli/cli_main.go
@@ -5,7 +5,6 @@ var Main struct {
 		Output string `alt:"o" desc:"Output directory relative to the pack working dir" default:"./build"`
 		Zip    bool   `alt:"z" desc:"Export data & resource packs as .zip files"`
 		Debug  bool   `alt:"d" desc:"Print verbose debug information"`
-		Cache  bool   `alt:"c" desc:"Use caching to prevent unnecessary computation"`
 	}
 	Args struct {
 		WorkDir *string
